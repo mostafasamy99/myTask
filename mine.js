2a1a4e1fbe6f4a074e0dcd8287c5161209ed74e2
mostafasamy99 <mostafamenshawy1999@gmail.com>
2019-08-30 21:20:32 +0200

    task

---
+++ mine.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    alert("انظر اسفل الصفحة للتسجيل") 
+  } 
+    else{
+        x.style.display="none";
+    }
+}
+
+
